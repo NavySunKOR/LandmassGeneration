e799e765db738f1b7fef974222e792c4d35f9fb2
NavySunKOR <dragunov2008@naver.com>
2021-01-21 09:47:41 +0900

    텍스쳐 생성기 업로드

---
+++ Procedural Landmass/Assets/TextureGenerator.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class TextureGenerator
+{
+
+    public static Texture2D DrawTexture(Color[] colors,int width, int height)
+    {
+        Texture2D texture = new Texture2D(width, height);
+        texture.SetPixels(colors);
+        texture.Apply();
+
+        return texture;
+    }
+}
